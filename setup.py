8eb453f174f3683a7cb676cf56d294aeb307d513
raptium <raptium@gmail.com>
2011-04-15 22:53:51 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "hitomi",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['lxml'],
+    
+    author = "raptium",
+    author_email = "raptium@gmail.com",
+    description = "Yet another Readability clone in Python."
+)
